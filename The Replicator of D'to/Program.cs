e095633cd88cb1aa67a21cf687cefb926993deaf
Robin Kelsick <robinkelsick@gmail.com>
2023-03-09 15:32:54 -0500

    2023.03.09 The Replicator of D'to

---
+++ The Replicator of D'to/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Title = "The Replicator of D'to";
+
+int[] numbers = new int[5];
+
+for (int i = 0 ; i < numbers.Length; i++)
+{
+    Console.WriteLine("Enter a Number");
+    int value = Convert.ToInt32(Console.ReadLine());
+
+    numbers[i] = value;
+}
+
+int[] copy = new int[numbers.Length];
+
+for (int i = 0 ;i < numbers.Length; i++)
+{
+    copy[i] = numbers[i];
+}
+
+for (int i = 0 ; i < numbers.Length; i++)
+    Console.WriteLine(copy[i]);
