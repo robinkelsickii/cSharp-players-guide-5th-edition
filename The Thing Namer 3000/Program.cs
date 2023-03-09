20c5fc61c72d6222eabf34a5447831bee213793f
Robin Kelsick <robinkelsick@gmail.com>
2023-03-09 12:06:15 -0500

    Release 2023.03.09 "The Thing Namer 3000"

---
+++ The Thing Namer 3000/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Title = "The Thing Namer 3000";
+
+Console.WriteLine("What type of thing is it?");
+string thingType = Console.ReadLine();
+
+Console.WriteLine("Okie Dokie. Give me one word you can desribe it with.");
+string description = Console.ReadLine();
+
+Console.WriteLine($"The {description} {thingType} of Doom 3000. I know, I know very original.");
