7ea9039ede183e1eea597c10caf842158a4afde1
Robin Kelsick II <robinkelsickii@gmail.com>
2023-03-09 12:14:10 -0500

    Delete ConsolasAndTelim(Challenge) directory

--- ConsolasAndTelim(Challenge)/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.Title = "Consolas and Telim";
-
-Console.WriteLine("The Bread is Done! \n Who is it for?");
-string customer = Console.ReadLine();
-
-Console.WriteLine($"Is there a {customer} here?!");
-
-
